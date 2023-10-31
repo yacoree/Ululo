133e63e253c2dda417c02420c230bf907e60145a
Юрий <@DESKTOP-EM4GTB2>
2023-11-01 00:23:20 +0300

    Добавьте файлы проекта.

---
+++ Ululo/Controls/MyTextBox.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ululo.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для MyTextBox.xaml
+    /// </summary>
+    public partial class MyTextBox : UserControl
+    {
+        public MyTextBox()
+        {
+            InitializeComponent();
+        }
+
+        public string Hint
+        {
+            get { return (string)GetValue(HitProperty); }
+            set { SetValue(HitProperty, value); }
+        }
+
+        public static readonly DependencyProperty HitProperty = DependencyProperty.Register
+            ("Hint", typeof(string), typeof(MyTextBox));
+    }
+}
---
+++ Ululo/Controls/StatElement.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ululo.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для StatElement.xaml
+    /// </summary>
+    public partial class StatElement : UserControl
+    {
+        public StatElement()
+        {
+            InitializeComponent();
+        }
+
+
+        public string Activity
+        {
+            get { return (string)GetValue(ActivityProperty); }
+            set { SetValue(ActivityProperty, value); }
+        }
+
+        public static readonly DependencyProperty ActivityProperty = DependencyProperty.Register
+            ("Hint", typeof(string), typeof(StatElement));
+
+        public string FirstPlase
+        {
+            get { return (string)GetValue(FirstPlaseProperty); }
+            set { SetValue(FirstPlaseProperty, value); }
+        }
+
+        public static readonly DependencyProperty FirstPlaseProperty = DependencyProperty.Register
+            ("Hint", typeof(string), typeof(StatElement));
+
+        public string SecondPlase
+        {
+            get { return (string)GetValue(SecondPlaseProperty); }
+            set { SetValue(SecondPlaseProperty, value); }
+        }
+
+        public static readonly DependencyProperty SecondPlaseProperty = DependencyProperty.Register
+            ("Hint", typeof(string), typeof(StatElement));
+
+        public string ThirdPlase
+        {
+            get { return (string)GetValue(ThirdPlaseProperty); }
+            set { SetValue(ThirdPlaseProperty, value); }
+        }
+
+        public static readonly DependencyProperty ThirdPlaseProperty = DependencyProperty.Register
+            ("Hint", typeof(string), typeof(StatElement));
+    }
+
+
+    }
